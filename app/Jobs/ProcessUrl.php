6d3a1231a05e43c4fc9a0eafb6555161762fb8d9
Carter Fort <carter.fort@gmail.com>
2016-08-13 10:29:37 -0400

    Use the url list to ensure that we don't go down an endless hole
    Only process pages right now; no images
    
    Recursively load all the pages loaded on each of these pages (using the `urls` array to prevent duplication or looping)

--- app/Jobs/ProcessUrl.php
+++ app/Jobs/ProcessUrl.php
@@ -3,7 +3,11 @@
 namespace App\Jobs;
 
 use App\Jobs\Job;
+use App\Events\UrlWasArchived;
+use App\Events\ResourceWasLoaded;
+use Illuminate\Support\Facades\File;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Queue\InteractsWithQueue;
 use Symfony\Component\DomCrawler\Crawler;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -37,26 +41,78 @@ class ProcessUrl extends Job implements ShouldQueue
      */
     public function handle()
     {
-        
-        $rootHtml = file_get_contents($this->url);
+        $this->parseUrl($this->url);
+        return ($this->urls);
+
+    }
+
+    protected function parseUrl($url)
+    {
+        event(new ResourceWasLoaded($url));
+        $rootHtml = file_get_contents($url);
 
         $crawler = new Crawler($rootHtml);
 
         $links = $crawler->filter('a');
+        $followLinks = [];
+
         foreach ($links as $link)
         {
             $href = $link->getAttribute('href');
-            if (str_contains($href, $this->url) && $this->url != $href){
-                $this->urls[] = str_replace($this->url, '', $href);
-            }
-
-            //1. Don't follow links we've already got
-            //2. Don't 
-
+            $this->addLink($href, $rootHtml, $followLinks);
         }
 
-        dd($this->urls);
+        foreach ($followLinks as $linkToFollow)
+        {
+            $this->parseUrl($linkToFollow);
+        }
 
-       die();
+        $this->storeHtmlForUrl($url, $rootHtml);
+
+        event(new UrlWasArchived($url, null));
+    }
+
+    protected function storeHtmlForUrl($url, $html)
+    {
+        if ( ! is_dir(storage_path('output')))
+        {
+            mkdir(storage_path('output'), 0755, false);
+        }
+
+        $url = str_replace($this->url."/", "", $url);
+        $url = rtrim($url, "/");
+        $pathComponents = explode("/", $url);
+
+        $currentPath = storage_path('output');
+
+        if ( $url != $this->url){
+            foreach ($pathComponents as $component)
+            {
+                $currentPath .= "/{$component}";
+                if ( ! is_dir($currentPath)){
+                    mkdir($currentPath, 0777, false);
+                }
+            }
+        }
+
+        $storagePath = "{$currentPath}/index.html";
+        $bytes = file_put_contents($storagePath, $html);
+    }
+
+    protected function addLink($href, $rootHtml, &$followLinks)
+    {
+        $pattern = '/(https?:\/\/.*\.(?:png|jpg))/';
+
+        $matches = [];
+        preg_match($pattern, $href, $matches);
+        if (count($matches) > 0) return;
+
+        if (str_contains($href, $this->url) && $this->url != $href){
+                if ( ! in_array($href, $this->urls))
+                {
+                    $followLinks[] = $href;
+                    $this->urls[] = $href;
+                }
+            }
     }
 }
